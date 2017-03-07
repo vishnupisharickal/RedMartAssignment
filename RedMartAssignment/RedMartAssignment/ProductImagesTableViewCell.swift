ced2bfa90fd480baca5159f867b6a63eaa53a064
Vishnu S <vishnupisharickal@gmail.com>
2017-03-07 20:46:43 +0530

    [Code modified]Pagination added for product images

--- RedMartAssignment/RedMartAssignment/ProductImagesTableViewCell.swift
+++ RedMartAssignment/RedMartAssignment/ProductImagesTableViewCell.swift
@@ -8,20 +8,85 @@
 
 import UIKit
 
-class ProductImagesTableViewCell: UITableViewCell {
+class ProductImagesTableViewCell: UITableViewCell, UIScrollViewDelegate {
     
-    @IBOutlet weak var productImageView: UIImageView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    var product: Product?
+    var imageViewFrame: CGRect = CGRect(x:0, y:0, width:0, height:0)
+    var imageViews = [UIImageView]()
     
     override func awakeFromNib() {
         
         super.awakeFromNib()
-        self.productImageView.clipsToBounds = true
+        scrollView.delegate = self
         
     }
 
     func setupCellFor(product: Product) {
+        
+        self.product = product
+        
+        configurePageControl()
+        
+        for index in 0..<product.images.count {
+            
+            imageViewFrame.origin.x = self.scrollView.frame.size.width * CGFloat(index)
+            imageViewFrame.size = self.scrollView.frame.size
+            self.scrollView.isPagingEnabled = true
+            
+            let productImageView = UIImageView(frame: imageViewFrame)
+            productImageView.clipsToBounds = true
+            productImageView.contentMode = .scaleAspectFill
+            
+            self.scrollView.addSubview(productImageView)
+            imageViews.append(productImageView)
+            productImageView.setImageFromURL(product.images[index], placeHolder: "PlaceHolder")
+            
+        }
+        
+        self.scrollView.contentSize = CGSize(width:self.scrollView.frame.size.width * CGFloat(product.images.count), height:self.scrollView.frame.size.height)
+        pageControl.addTarget(self, action: #selector(ProductImagesTableViewCell.changePage(sender:)), for: UIControlEvents.valueChanged)
+        
+    }
     
-        productImageView.setImageFromURL(product.coverImage!, placeHolder: "PlaceHolder")
+    func configurePageControl() {
+       
+        self.pageControl.numberOfPages = (product?.images.count)!
+        self.pageControl.currentPage = 0
+        self.pageControl.tintColor = UIColor.red
+        self.pageControl.pageIndicatorTintColor = UIColor.black
+        self.pageControl.currentPageIndicatorTintColor = UIColor.green
+        
+        if product?.images.count == 1 {
+            
+            pageControl.isHidden = true
+            scrollView.isScrollEnabled = false
+            
+        }
+        
+    }
+    
+    func changePage(sender: AnyObject) -> () {
+        
+        let x = CGFloat(pageControl.currentPage) * scrollView.frame.size.width
+        scrollView.setContentOffset(CGPoint(x:x, y:0), animated: true)
+        
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        
+        let pageNumber = Int (round(scrollView.contentOffset.x / scrollView.frame.size.width))
+        
+        if pageNumber < (product?.images.count)! {
+            
+            let imageView = imageViews[pageNumber]
+            imageViewFrame.origin.x = self.scrollView.frame.size.width * CGFloat(pageNumber)
+            imageViewFrame.size = self.scrollView.frame.size
+            imageView.frame = imageViewFrame
+            pageControl.currentPage = Int(pageNumber)
+            
+        }
         
     }
     
